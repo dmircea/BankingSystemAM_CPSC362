757d87baa5c89e78f972130f60a2cda479032dcc
dmircea <dmircea4you@gmail.com>
2018-02-15 16:49:57 -0800

    Fixed with new correct upload.

--- Address.h
+++ Address.h
@@ -1,1 +1,12 @@
 #pragma once
+
+//This struct contains the data for the address of the customer
+struct Address
+{
+	int number = 0;
+	int zipcode = 0;
+	std::string street = "";
+	std::string city = "";
+	std::string state = "";
+
+};
